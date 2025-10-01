9ec5b22ebc4559de213f9e62b485d75db56233ad
Andy Kreling <krelinga@gmail.com>
2025-10-01 03:53:27 +0000

    implement many additional operations for string and ~string.

--- string.go
+++ string.go
@@ -1,5 +1,7 @@
 package typemap
 
+import "iter"
+
 type ForStringLike[T ~string] struct {
 	StringFunc[T]
 	DefaultCompare[T]
@@ -10,11 +12,45 @@ func (s ForStringLike[T]) Length(v T) int {
 	return len(v)
 }
 
-// TODO: implement the following on String:
-// - HasKey(string, int)
-// - GetValue(string, int)
-// - AllKeys(string)
-// - AllValues(string)
-// - AllKeysAndValues(string)
+func (s ForStringLike[T]) HasKey(v T, key int) bool {
+	return key >= 0 && key < len(v)
+}
+
+func (s ForStringLike[T]) GetValue(v T, key int) (rune, bool) {
+	if !s.HasKey(v, key) {
+		return 0, false
+	}
+	return rune(v[key]), true
+}
+
+func (s ForStringLike[T]) AllKeys(v T) iter.Seq[int] {
+	return func(yield func(int) bool) {
+		for i := range v {
+			if !yield(i) {
+				return
+			}
+		}
+	}
+}
+
+func (s ForStringLike[T]) AllValues(v T) iter.Seq[rune] {
+	return func(yield func(rune) bool) {
+		for _, r := range v {
+			if !yield(r) {
+				return
+			}
+		}
+	}
+}
+
+func (s ForStringLike[T]) AllKeysAndValues(v T) iter.Seq2[int, rune] {
+	return func(yield func(int, rune) bool) {
+		for i, r := range v {
+			if !yield(i, r) {
+				return
+			}
+		}
+	}
+}
 
 type ForString = ForStringLike[string]
--- string_test.go
+++ string_test.go
@@ -9,10 +9,16 @@ import (
 func TestStringLike(t *testing.T) {
 	t.Run("implements", func(t *testing.T) {
 		type myString string
-		assertImplements[typemap.ForStringLike[myString], typemap.String[myString]](t)
-		assertImplements[typemap.ForStringLike[myString], typemap.Compare[myString]](t)
-		assertImplements[typemap.ForStringLike[myString], typemap.Order[myString]](t)
-		assertImplements[typemap.ForStringLike[myString], typemap.Length[myString]](t)
+		type Got = typemap.ForStringLike[myString]
+		assertImplements[Got, typemap.String[myString]](t)
+		assertImplements[Got, typemap.Compare[myString]](t)
+		assertImplements[Got, typemap.Order[myString]](t)
+		assertImplements[Got, typemap.Length[myString]](t)
+		assertImplements[Got, typemap.HasKey[myString, int]](t)
+		assertImplements[Got, typemap.GetValue[myString, int, rune]](t)
+		assertImplements[Got, typemap.AllKeys[myString, int]](t)
+		assertImplements[Got, typemap.AllValues[myString, rune]](t)
+		assertImplements[Got, typemap.AllKeysAndValues[myString, int, rune]](t)
 	})
 }
 
@@ -22,5 +28,10 @@ func TestString(t *testing.T) {
 		assertImplements[typemap.ForString, typemap.Compare[string]](t)
 		assertImplements[typemap.ForString, typemap.Order[string]](t)
 		assertImplements[typemap.ForString, typemap.Length[string]](t)
+		assertImplements[typemap.ForString, typemap.HasKey[string, int]](t)
+		assertImplements[typemap.ForString, typemap.GetValue[string, int, rune]](t)
+		assertImplements[typemap.ForString, typemap.AllKeys[string, int]](t)
+		assertImplements[typemap.ForString, typemap.AllValues[string, rune]](t)
+		assertImplements[typemap.ForString, typemap.AllKeysAndValues[string, int, rune]](t)
 	})
 }
